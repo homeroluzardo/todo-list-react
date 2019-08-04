aec89d8c06252450c3c2936f4b371716811e6636
Homero Luzardo <homeroeluzardo@gmail.com>
2019-08-04 13:23:06 +0000

    Todo List with React

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,21 +5,58 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tasks: [
+				"Make the bed",
+				"Wash my hands",
+				"Walk the dog",
+				"Eat",
+				"Go shopping"
+			]
+		};
+	}
+
+	addItem(event) {
+		if (event.key === "Enter") {
+			this.setState({
+				tasks: this.state.tasks.concat(event.target.value)
+			});
+		}
+	}
+
+	removeItem(erase) {
+		let temporal = this.state.tasks.filter(item => item !== erase);
+		this.setState({ tasks: temporal });
+		//console.log(temporal);
+	}
+
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+			<div id="lista">
+				<h1>todos</h1>
+				<input
+					onKeyPress={event => this.addItem(event)}
+					className="newTask"
+					type="text"
+					placeholder="What needs to be done?"
+				/>
+				<ul className="list-group">
+					{this.state.tasks.map((item, index) => {
+						return (
+							<li className="list-group-item" key={index}>
+								{item}{" "}
+								<span onClick={() => this.removeItem(item)}>
+									&#10005;
+								</span>
+							</li>
+						);
+					})}
+					<li className="list-group-item" id="itemsLeft">
+						{this.state.tasks.length + " items left"}
+					</li>
+				</ul>
 			</div>
 		);
 	}
